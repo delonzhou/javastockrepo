e189bbe05ee205c6e2c57eb9cec96a3e4de93d0c
ori <ori@PC.privatebox>
2015-06-01 14:20:51 +0300

    Exercise 08 finished

---
+++ src/com/oriborohov/javacourse/servlet/InitServlet.java
@@ -0,0 +1,20 @@
+package com.oriborohov.javacourse.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import org.algo.service.ServiceManager;
+
+import com.oriborohov.javacourse.service.PortfolioManager;
+
+@SuppressWarnings("serial")
+public class InitServlet extends HttpServlet
+{
+	@Override
+	public void init() throws ServletException 
+	{
+		super.init();
+		PortfolioManager pm = new PortfolioManager();
+		ServiceManager.setPortfolioManager(pm);
+	}
+}
